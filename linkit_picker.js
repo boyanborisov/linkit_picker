7d20dca0252e3f2c995a878ead69b665ba80a111
Emil Stjerneman <emil@stjerneman.com>
2011-01-28 13:07:35 +0000

    First file commit

---
+++ linkit_picker.js
@@ -0,0 +1,18 @@
+// $Id$
+
+Drupal.behaviors.linkit_picker_buttons = function(context) {
+  $('.linkit_picker_button', context).click(function() {
+    var type = $(this).attr('id').replace('edit-','');
+    $('#linkit-picker-container .view-container').hide();
+    $('#linkit-picker-container .view-linkit-picker-' + type).parent().toggle();
+    return false;
+  });
+}
+
+Drupal.behaviors.linkit_picker = function(context) {
+  $('.view-display-id-default tr td', context).click(function() {
+    var type = $(this).parent().find('.views-field-linkit-picker');
+    $('#edit-link').val($.trim(type.text()));
+    //$('#edit-insert').trigger('click');
+  });
+}
