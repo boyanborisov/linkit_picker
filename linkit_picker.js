a82abe4f8e14f27c582c43efbd3f93ee6fd60505
Boyan Borisov <boyanb@propeople.dk>
2014-06-02 19:16:02 +0300

    Add support to Linkit v3

--- linkit_picker.js
+++ linkit_picker.js
@@ -2,24 +2,26 @@
 
 	Drupal.behaviors.linkit_picker_buttons = {
 		attach: function(context) {
-			$('.linkit_picker_button', context).click(function() {
-				var type = $(this).attr('id').replace('edit-','');
+			$('a.linkit-picker-button:not(.linkit-processed)').addClass('linkit-processed').click(function() {
+			  $('#linkit-picker-container a.linkit-picker-button').removeClass('selected');
+			  $(this).addClass('selected');
+				var viewname = $(this).data('viewname');
 				$('#linkit-picker-container .view-container').hide();
-				$('#linkit-picker-container .view-linkit-picker-' + type).parents('.view-container').toggle();
+				$('#linkit-picker-container .view-linkit-picker-' + viewname).toggle();
 				return false;
 			});
-			$('#linkit-picker-container .view-container:first').show();
+			$('#linkit-picker-container .view-container:first', context).show();
+			$('#linkit-picker-container a.linkit-picker-button:first', context).addClass('selected');
 		}
 	}
-	
+
 	Drupal.behaviors.linkit_picker = {
 		attach: function(context) {
-			$('.view-display-id-default tr td', context).click(function() {
+			$('#linkit-picker-container .view-container tr td:not(.linkit-row-processed)').addClass('linkit-row-processed').click(function() {
 				var type = $(this).parent().find('.views-field-linkit-picker');
 				$('#edit-linkit-path').val($.trim(type.text())).focus();
-				Drupal.linkit.dialog.requiredFieldsValidation();
+				Drupal.linkit.requiredFieldsValidation();
 				$('html, body').animate({scrollTop:0}, 200);
-				$('#edit-browser-wrapper.collapsible legend a').click();
 			});
 		}
 	}
